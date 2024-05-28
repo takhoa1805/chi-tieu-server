cf12888880e4b5ed6aa4dc972e604d4b13f91846
takhoa <takhoa1805@gmail.com>
2024-05-28 11:32:34 +0700

    wallet done

--- controllers/WalletController.js
+++ controllers/WalletController.js
@@ -44,6 +44,7 @@ class WalletController {
 
             return {
                 message:'Wallet created successfully',
+                _id:result._id,
                 user:result.user,
                 name:result.name,
                 balance:result.balance,
@@ -56,7 +57,7 @@ class WalletController {
 
     }
 
-
+    // GET ALL WALLET HANDLER
     async getWallets(input){
         const {user_id} = input;
         try{
@@ -71,6 +72,130 @@ class WalletController {
         }
     }
 
+    // GET WALLET BY NAME HANDLER
+    async getWalletByName(input){
+        const {user_id,name} = input;
+        try{
+            const result = await WalletModel.findOne({user:user_id,name:name});
+            return {
+                wallet: result
+            }
+        }   catch(error){
+            throw (error);
+        }
+    }
+
+    // GET WALLET BY ID HANDLER
+    async getWalletById(input){
+        const {user_id,id} = input;
+        try{
+            const result = await WalletModel.findOne({user:user_id,_id:id});
+            return {
+                wallet: result
+            }
+        }   catch(error){
+            throw (error);
+        }
+    }
+
+    // UPDATE WALLET INFORMATION HANDLER
+    async updateWallet(input){
+        const {_id,name,balance,detail,user_id} = input;
+        try{
+            const wallet = await WalletModel.findOne({user:user_id,_id:_id});
+
+            if (!wallet){
+                console.log("Wallet not found");
+                return {
+                    error:{
+                        message:'Wallet not found'
+                    }
+                }
+            }
+
+            
+            // If user want to change wallet's name, check for name that exists
+            if (name && name != wallet.name){
+                const existedWalletName = await WalletModel.findOne({user:user_id,name:name});
+                if(existedWalletName){
+                    console.log("New wallet name is not available");
+                    return {
+                        error:{
+                            message:'New wallet name has existed'
+                        }
+                    }
+                }
+            }
+
+            // update fields
+            wallet.name = (name) ? name : wallet.name;
+            wallet.balance = (balance) ? balance : wallet.balance;
+            wallet.detail = (detail) ? detail : wallet.detail;
+
+            // save result
+            const result = await wallet.save();
+
+
+            return {
+                wallet: result
+            }
+        }   catch(error){
+            throw (error);
+        }
+    }
+
+    // DELETE WALLET HANDLER
+    async deleteWallet(input){
+        const {_id,user_id} = input;
+        try{
+            const wallet = await WalletModel.findOne({user:user_id,_id:_id});
+            const user = await UserModel.findOne({_id:user_id});
+
+            // verify user
+            if (!user){
+                console.log("User not found");
+                return {
+                    error:{
+                        message:'User not found'
+                    }
+                };
+            }
+            // verify wallet
+            if (!wallet){
+                console.log("Wallet not found");
+                return {
+                    error:{
+                        message:'Wallet not found'
+                    }
+                }
+            }
+
+            // delete wallet model
+            const result = await wallet.deleteOne();
+            // update user wallets model
+            if (result.acknowledged){
+                user.wallets.pop(_id);
+                await user.save();
+            }   else {
+                return {
+                    error:{
+                        message:'delete failed',
+                        result:result
+                    }
+                }
+            }
+
+
+            return {
+                message:'deleted'
+            }
+
+
+        }   catch(error){
+            throw (error);
+        }
+    }
+    
 }
 
 module.exports = new WalletController();
--- routes/wallets.js
+++ routes/wallets.js
@@ -36,6 +36,84 @@ router.get('/', authorize_user,async (req, res, next)  => {
 
 });
 
+// GET WALLET WITH NAME
+router.get('/find/:name',authorize_user,async (req,res,next) =>{
+  const user_id = req.user._id;
+  const name = req.params.name;
+  try{
+    if (!user_id){
+      console.log("Error: Cannot find user id");
+      return res.status(404).json({message:'User not found'});
+    }
+    
+    if (!name){
+      return res.status(400).json({
+        error:{
+          message:'Invalid request'
+        }
+      });
+    }
+
+    const data = await WalletController.getWalletByName({user_id,name});
+    
+    if (!data){
+      return res.status(400).json({
+        error:{
+          message:'Invalid request'
+        }
+      });
+    } else if (data.error){
+      console.log(data.error);
+      return res.status(400).json(data);
+    }
+
+    return res.status(200).json(data);
+  } catch(error){
+    console.log("Error happens: " + error);
+    return res.status(500).json({message:'Error happens'});
+  }
+
+});
+
+
+// GET WALLET WITH ID
+router.get('/find_id/:id',authorize_user,async (req,res,next) =>{
+  const user_id = req.user._id;
+  const id = req.params.id;
+  try{
+    if (!user_id){
+      console.log("Error: Cannot find user id");
+      return res.status(404).json({message:'User not found'});
+    }
+    if (!id){
+      return res.status(400).json({
+        error:{
+          message:'Invalid request'
+        }
+      });
+    }
+
+    const data = await WalletController.getWalletById({user_id,id});
+    
+    if (!data){
+      return res.status(400).json({
+        error:{
+          message:'Invalid request'
+        }
+      });
+    } else if (data.error){
+      console.log(data.error);
+      return res.status(400).json(data);
+    }
+
+    return res.status(200).json(data);
+  } catch(error){
+    console.log("Error happens: " + error);
+    return res.status(500).json({message:'Error happens'});
+  }
+
+});
+
 // CREATE NEW WALLET
 router.post('/create',authorize_user,async(req,res,next) =>{
   const user_id = req.user._id;
@@ -63,7 +141,9 @@ router.post('/create',authorize_user,async(req,res,next) =>{
       detail = 'None';
     }
 
-
+    if (name){
+      name = name.replace(/\s*$/,'');
+    }
     const data = await WalletController.createWallet({name,balance,detail,user_id});
     
     if (!data){
@@ -85,6 +165,80 @@ router.post('/create',authorize_user,async(req,res,next) =>{
     return res.status(500).json({message:'Error happens'});
   }
 
-})
+});
+
+// UPDATE NEW WALLET
+router.post('/update/',authorize_user,async(req,res,next) =>{
+  const user_id = req.user._id;
+  var {_id,name,balance,detail} = req.body;
+  try{
+    if (!user_id){
+      console.log("Error: Cannot find user id");
+      return res.status(404).json({message:'User not found'});
+    }
+
+    if (name){
+      name = name.replace(/\s*$/,'');
+    }
+    const data = await WalletController.updateWallet({_id,name,balance,detail,user_id});
+    
+    if (!data){
+      return res.status(400).json({
+        error:{
+          message:'Invalid request'
+        }
+      });
+    } else if (data.error){
+      console.log(data.error);
+      return res.status(400).json(data);
+    }
+
+    return res.status(200).json(data);
+
+
+  } catch(error){
+    console.log("Error happens: " + error);
+    return res.status(500).json({message:'Error happens'});
+  }
+
+});
+
+
+// DELETE WALLET
+router.post('/delete/',authorize_user,async(req,res,next) =>{
+  const user_id = req.user._id;
+  var {_id} = req.body;
+  try{
+    if (!user_id){
+      console.log("Error: Cannot find user id");
+      return res.status(404).json({message:'User not found'});
+    }
+
+    const data = await WalletController.deleteWallet({_id,user_id});
+    
+    if (!data){
+      return res.status(400).json({
+        error:{
+          message:'Invalid request'
+        }
+      });
+    } else if (data.error){
+      console.log(data.error);
+      return res.status(400).json(data);
+    }
+
+    return res.status(200).json(data);
+
+
+  } catch(error){
+    console.log("Error happens: " + error);
+    return res.status(500).json({message:'Error happens'});
+  }
+
+});
+
+
+
+
 
 module.exports = router;
